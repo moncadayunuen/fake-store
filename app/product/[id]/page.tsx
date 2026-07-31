b126792bd6106b31739d44d6ee0475117317c0c1
Yun <50856260+moncadayunuen@users.noreply.github.com>
2026-07-31 12:54:57 -0600

    .

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,5 +1,14 @@
 import ProductDetail from "./productDetail";
 
+export async function generateStaticParams() {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const products = await res.json();
+
+    return products.map((product: { id: number }) => ({
+        id: product.id.toString(),
+    }));
+}
+
 export default function Page() {
     return <ProductDetail />;
 }
