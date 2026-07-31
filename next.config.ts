d298a66cf03007222898d0200888a3a276c25b3a
Yun <50856260+moncadayunuen@users.noreply.github.com>
2026-07-31 12:50:09 -0600

    pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "export",
+    basePath: "/fake-store",
+    assetPrefix: "/fake-store",
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
